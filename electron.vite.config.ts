3037a780fb926e773864b78edf2af4802424d764
PokeHaven Dev <seoltaanging@gmail.com>
2026-06-17 07:14:28 +0900

    chore(app): add electron-vite + svelte toolchain

---
+++ electron.vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'node:path';
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  renderer: {
+    root: resolve('src/renderer'),
+    build: {
+      rollupOptions: {
+        input: resolve('src/renderer/index.html'),
+      },
+    },
+    plugins: [svelte()],
+  },
+});
